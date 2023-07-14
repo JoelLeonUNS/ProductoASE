33a9d2771bf0d1e5f9c051b03ac87d4c8aee2592
AngieUNS <202014055@uns.edu.pe>
2023-07-14 16:16:56 -0500

    //Cargar BASE DE DATOS .....

--- src/BaseDeDatos/BD.java
+++ src/BaseDeDatos/BD.java
@@ -1,5 +1,6 @@
 package BaseDeDatos;
 
+import consultas.ConsultaMedica;
 import examenes.Examen;
 import historias.HistoriaClinica;
 import java.util.HashMap;
@@ -13,41 +14,49 @@ public class BD {
     private static int idUsuario;
     private static int idExamen;
     private static int idHistoriaClinica;
+    private static int idConsulta;
     private static final Map<Integer, Medico> medicos = new HashMap<>();
     private static final Map<Integer, Usuario> usuarios = new HashMap<>();
     private static final Map<Integer, Examen> examenes = new HashMap<>();
-    private static final Map<Integer, HistoriaClinica> historia= new HashMap<>();
+    private static final Map<Integer, HistoriaClinica> historia = new HashMap<>();
+    private static final Map<Integer, ConsultaMedica> consulta = new HashMap<>();
 
     public static Map<Integer, Medico> getMedicos() {
         return medicos;
     }
-    
+
     public static int IdMedico() {
         return idMedico++;
     }
-    
+
     public static Map<Integer, Usuario> getUsuarios() {
         return usuarios;
     }
-    
-    public static int IdUsuario(){
+
+    public static int IdUsuario() {
         return idUsuario++;
     }
-    
+
     public static Map<Integer, Examen> getExamenes() {
         return examenes;
     }
-    
-    public static int IdExamen(){
+
+    public static int IdExamen() {
         return idExamen++;
     }
 
     public static Map<Integer, HistoriaClinica> getHistoria() {
         return historia;
     }
-    
-        public static int IdHistoriaClinica(){
+
+    public static int IdHistoriaClinica() {
         return idHistoriaClinica++;
     }
-            
+
+    public static Map<Integer, ConsultaMedica> getConsulta() {
+        return consulta;
+    }
+    public static int IdConsulta() {
+        return idConsulta++;
+    }
 }
