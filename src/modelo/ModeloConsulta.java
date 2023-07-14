5ac1ab3a714b6158fcbe1f33605286ee95b4f87a
AlexBD27 <202014034@uns.edu.pe>
2023-07-14 15:26:49 -0500

    Actualización pExamen - Consulta

--- src/modelo/ModeloConsulta.java
+++ src/modelo/ModeloConsulta.java
@@ -1,41 +1,26 @@
 package modelo;
 
 import consultas.ConsultaMedica;
-import java.util.Scanner;
 
 public class ModeloConsulta {
 
-    private final Scanner input = new Scanner(System.in);
+    private ConsultaMedica consulta;
+    private int idConsulta;
 
-    public ConsultaMedica registrar() {
-        ConsultaMedica nuevaConsulta = new ConsultaMedica();
-        setDatos(nuevaConsulta);
-        
-        System.out.println(nuevaConsulta);
-        
-        return nuevaConsulta;
+    public ModeloConsulta() {
+        this.consulta = new ConsultaMedica();
     }
 
-    private void setDatos(ConsultaMedica consulta) {
-        System.out.print("\nFecha: ");
-        consulta.setFecha(input.next());
-        System.out.print("Hora: ");
-        consulta.setHora(input.next());
-        System.out.print("Edad: ");
-        consulta.setEdad(input.nextInt());
-        System.out.print("Motivo: ");
-        input.nextLine();
-        consulta.setMotivo(input.nextLine());
-        System.out.print("Tiempo de Enfermedad: ");
-        consulta.setTiempoEnfermedad(input.nextInt());
-        System.out.print("Apetito: ");
-        consulta.setApetito(input.next());
-        System.out.print("Sueño: ");
-        consulta.setSueño(input.next());
-        System.out.print("Sed: ");
-        consulta.setSed(input.next());
-        System.out.print("Estado de Animo: ");
-        consulta.setEstadoAnimo(input.next());
+    private void setDatosConsulta(String fecha, String hora, int edad, int tiempoEnfermedad, String apetito, String sueño, String sed, String estadoAnimo, String motivo) {
+        consulta.setFecha(fecha);
+        consulta.setHora(hora);
+        consulta.setEdad(edad);
+        consulta.setTiempoEnfermedad(tiempoEnfermedad);
+        consulta.setApetito(apetito);
+        consulta.setSueño(sueño);
+        consulta.setEstadoAnimo(estadoAnimo);
+        consulta.setSed(sed);
+        consulta.setMotivo(motivo);
     }
 
 }
--- src/presentador/PresentadorExamen.java
+++ src/presentador/PresentadorExamen.java
@@ -1,10 +1,46 @@
 
 package presentador;
 
+import modelo.ModeloConsulta;
+import modelo.ModeloExamen;
+import modelo.ModeloHistoriaClinica;
+
 /**
  *
  * @author USER
  */
 public class PresentadorExamen {
+    private ModeloExamen modeloExamen = new ModeloExamen();
+    private ModeloConsulta modeloConsulta = new ModeloConsulta();
+    private ModeloHistoriaClinica modeloHistoriaClinica = new ModeloHistoriaClinica();
+
+    public PresentadorExamen() {
+    }
+
+    public ModeloExamen getModeloExamen() {
+        return modeloExamen;
+    }
+
+    public void setModeloExamen(ModeloExamen modeloExamen) {
+        this.modeloExamen = modeloExamen;
+    }
+
+    public ModeloConsulta getModeloConsulta() {
+        return modeloConsulta;
+    }
+
+    public void setModeloConsulta(ModeloConsulta modeloConsulta) {
+        this.modeloConsulta = modeloConsulta;
+    }
+
+    public ModeloHistoriaClinica getModeloHistoriaClinica() {
+        return modeloHistoriaClinica;
+    }
+
+    public void setModeloHistoriaClinica(ModeloHistoriaClinica modeloHistoriaClinica) {
+        this.modeloHistoriaClinica = modeloHistoriaClinica;
+    }
+    
+    
     
 }
